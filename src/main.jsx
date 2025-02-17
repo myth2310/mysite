d643de58a988521829872cb1bef173867543bb14
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2025-02-17 18:12:08 +0700

    up mysite2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
-import './dist/css/main.css'
+
+import './css/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
